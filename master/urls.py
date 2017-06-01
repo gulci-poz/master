347cae3fbd3724481e3b632bbf8837d4f1312b10
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-06-01 14:02:06 +0200

    2016-06-01 dynamic views

--- master/urls.py
+++ master/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import hello
+from .views import current_datetime, hello, hours_ahead
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', hello),
     url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
--- master/views.py
+++ master/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from datetime import datetime, timedelta
+
+from django.http import Http404, HttpResponse
 
 
 def hello(request):
-    return HttpResponse('hello')
+    return HttpResponse(f'hello {request.method}')
+
+
+def current_datetime(request):
+    now = datetime.now()
+    html = f'<html><body>It is now {now}.</body></html>'
+    return HttpResponse(html)
+
+
+def hours_ahead(request, offset):
+    # loose coupling - urlconf sprawdza wzorzec, ale nie robimy założenia,
+    # że zawsze tak będzie, widok tak naprawdę nie wie co dostaje
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+
+    dt = datetime.now() + timedelta(hours=offset)
+    html = f'<html><body>In {offset} hours, it will be {dt}.</body></html>'
+    # debugowanie ala print
+    # assert False
+    return HttpResponse(html)
